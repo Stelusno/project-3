64e4355bb2b74a362032f8b210648b1ac6657596
Stelusno <stelusno@gmail.com>
2023-08-29 21:09:46 -0400

    Before deploy

--- movies/settings.py
+++ movies/settings.py
@@ -125,7 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATICFILES_DIRS = [BASE_DIR / 'static/',]
 STATIC_URL = 'static/'
+STATIC_ROOT = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
